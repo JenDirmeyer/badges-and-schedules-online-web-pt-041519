355f78449f36ed9220a4ea0f0e74a291d2b0d980
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-10 01:56:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array =[]
+  array.each {|name| badge_array<< "Hello, my name is #{name}."}
+  badge_array
+end
+
+def assign_rooms(array)
+  array.collect.with_index do |name, i|
+    "Hello, #{name}! You'll be assigned to room #{i + 1}!"
+  end
+end  
+
+def printer(array)
+  batch_badge_creator.each {|badge| puts badge}
+  assign_rooms(array)
+    array.each {|assignment| puts assignment}
+end  
